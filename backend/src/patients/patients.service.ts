6b0a04218d60bc737792ee4e7d48064139d10840
julieth <jperdomo@iegabo.edu.co>
2026-07-05 23:35:37 -0500

    feat: modulo patients and CRUD

--- backend/src/patients/patients.service.ts
+++ backend/src/patients/patients.service.ts
@@ -1,4 +1,51 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { CreatePatientDto } from './dto/create-patient.dto';
+import { UpdatePatientDto } from './dto/update-patient.dto';
 
 @Injectable()
-export class PatientsService {}
+export class PatientsService {
+
+  constructor(
+    private prisma: PrismaService,
+  ) {}
+
+  async create(createPatientDto: CreatePatientDto){
+    return await this.prisma.patient.create({
+        data: createPatientDto
+    })
+  }
+
+  async findAll(){
+    return this.prisma.patient.findMany({
+        orderBy: { createdAt: 'desc'}
+    })
+  }
+
+  async findOne(id: string){
+    const patient =  await this.prisma.patient.findUnique({
+        where:{id}
+    })
+
+    if(!patient){
+        throw new NotFoundException('Paciente no encontrado')
+    }
+    return patient
+  }
+
+  async update(id: string, updatePatientDto: UpdatePatientDto){
+    await this.findOne(id)
+
+    return this.prisma.patient.update({
+        where:{id }, data: updatePatientDto
+    })
+  }
+
+  async remove(id: string){
+    await this.findOne(id)
+
+    return this.prisma.patient.delete({
+        where:{id}
+    })
+  }
+}
