5118eb44921bf391a8438c2c493b955da7f1d378
julieth <jperdomo@iegabo.edu.co>
2026-07-05 19:06:31 -0500

    feat:conflicto version prisma

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PatientsModule } from './patients/patients.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [PatientsModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
